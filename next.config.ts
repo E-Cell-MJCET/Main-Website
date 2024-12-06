ee9c2e72bef502a0f28395997b39a452764a43f5
Mohammed Abid Nafi <abid.nafi04@gmail.com>
2024-12-07 02:09:41 +0530

    feat: membership

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,12 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ["www.ecellmjcet.com", "localhost"], // Add your external image domains here
+    domains: [
+      "www.ecellmjcet.com",
+      "localhost",
+      "img.freepik.com",
+      "www.baass.com",
+    ], // Add your external image domains here
   },
 };
 
