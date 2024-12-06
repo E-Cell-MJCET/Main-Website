7864cbb38797e7244d10ea6e90a0557f4e338e8b
Mohammed Abid Nafi <abid.nafi04@gmail.com>
2024-12-07 00:22:33 +0530

    fix

--- app/blogs/page.tsx
+++ app/blogs/page.tsx
@@ -1,14 +1,20 @@
 import React from "react";
 
 import { SBS } from "@/components/blogs/sbs";
+import Navbar from "@/components/LandingPage/Navbar";
+import Footer from "@/components/LandingPage/Footer";
 
 const BlogPage = () => {
   return (
-    <div className="flex w-full items-center justify-center">
-      {/* <TRE /> */}
-      {/* <VBI/> */}
-      {/* <OFE/> */}
-      <SBS />
+    <div>
+      <Navbar />
+      <div className="flex w-full items-center justify-center">
+        {/* <TRE /> */}
+        {/* <VBI/> */}
+        {/* <OFE/> */}
+        <SBS />
+      </div>
+      <Footer />
     </div>
   );
 };
