d9ac7366d7618d22cef9704aaa4c73306ef85c63
Mohammed Abid Nafi <abid.nafi04@gmail.com>
2025-04-14 04:24:14 +0530

    feat: add footer

--- app/hackcelerate/page.tsx
+++ app/hackcelerate/page.tsx
@@ -1,3 +1,23 @@
+import About from "@/components/hack-celerate/About";
+import FaqSection from "@/components/hack-celerate/faqs";
+import Hero from "@/components/hack-celerate/Hero";
+import Navbar from "@/components/hack-celerate/NavBar";
+import Organisers from "@/components/hack-celerate/organisers";
+import SponsorUs from "@/components/hack-celerate/sponsor-contact-us";
+import TimeLine1 from "@/components/hack-celerate/TimeLine1";
+import Cursor from "@/components/ui/cursor";
+
 export default function HackceleratePage() {
-  return <div>hackcelerate page</div>;
+  return (
+    <div>
+      <Cursor />
+      <Navbar />
+      <Hero />
+      <About />
+      <TimeLine1 />
+      <Organisers />
+      <SponsorUs />
+      <FaqSection />
+    </div>
+  );
 }
