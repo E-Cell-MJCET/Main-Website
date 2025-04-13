bbd3b348a640688fa8ab6e894f9de4c134a57318
Mohammed Abid Nafi <abid.nafi04@gmail.com>
2025-04-14 04:02:58 +0530

    clean: faq branch

--- app/hackcelerate/page.tsx
+++ app/hackcelerate/page.tsx
@@ -1,15 +1,3 @@
-import FaqSection from "@/components/hack-celerate/faqs";
-import Organisers from "@/components/hack-celerate/organisers";
-import SponsorUs from "@/components/hack-celerate/sponsor-contact-us";
-
-const HackceleratePage = () => {
-  return (
-    <div>
-      <Organisers />
-      <SponsorUs />
-      <FaqSection />
-    </div>
-  );
-};
-
-export default HackceleratePage;
+export default function HackceleratePage() {
+  return <div>hackcelerate page</div>;
+}
