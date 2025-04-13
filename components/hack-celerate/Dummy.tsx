34a9f1f5062f45462af7fbee53047e4d6fa1f6a7
syed abdul Muneeb <119971617+AbdulMuneebSyed@users.noreply.github.com>
2025-04-14 04:00:22 +0530

    feat/hack-celerate-hero (#30)
    * feat:hack-celeeate/hero
    
    * update:hack-celerate
    
    * feat: add sticky cursor
    
    * fix: lint error
    
    ---------
    
    Co-authored-by: Mohammed Abid Nafi <abid.nafi04@gmail.com>

---
+++ components/hack-celerate/Dummy.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Dummy = () => {
+  return (
+    <div className="h-screen w-screen bg-[#121212]" id="Dummy">
+      Dummy
+    </div>
+  );
+};
+
+export default Dummy;
