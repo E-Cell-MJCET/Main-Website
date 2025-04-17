be03b88c41523cc73d0f102b3baaa0cb0206151a
AbdulMuneebSyed <samuneeb786@gmail.com>
2025-04-17 18:57:03 +0530

    feat:help button added

--- components/hack-celerate/registration-modal.tsx
+++ components/hack-celerate/registration-modal.tsx
@@ -629,7 +629,14 @@ export function RegistrationModal({
                       </ul>
                     </div>
                   </div>
-                  <div className="mt-8 flex justify-end">
+                  <div className="mt-8 flex justify-around">
+                    <Link
+                      href={"https://chat.whatsapp.com/DOViTi8NHvH0LnapNsznoX"}
+                      type="button"
+                      className="flex items-center space-x-2 rounded-lg bg-gray-700 px-6 py-3 font-silkscreen text-white transition-all hover:bg-gray-600"
+                    >
+                      Need Help ?
+                    </Link>
                     <button
                       type="button"
                       onClick={nextStep}
@@ -711,7 +718,14 @@ export function RegistrationModal({
                       )}
                     />
                   </div>
-                  <div className="mt-8 flex justify-center">
+                  <div className="mt-8 flex justify-around">
+                    <Link
+                      href={"https://chat.whatsapp.com/DOViTi8NHvH0LnapNsznoX"}
+                      type="button"
+                      className="flex items-center space-x-2 rounded-lg bg-gray-700 px-6 py-3 font-silkscreen text-white transition-all hover:bg-gray-600"
+                    >
+                      Need Help ?
+                    </Link>
                     <button
                       type="button"
                       onClick={nextStep}
@@ -1028,7 +1042,7 @@ export function RegistrationModal({
                     />
                   </div>
                   {isTeam ? (
-                    <div className="mt-8 flex justify-between">
+                    <div className="mt-8 flex flex-col items-center gap-4 md:flex-row md:justify-around">
                       <button
                         type="button"
                         onClick={prevStep}
@@ -1037,17 +1051,53 @@ export function RegistrationModal({
                         <ChevronLeft size={18} />
                         <span>BACK</span>
                       </button>
+                      <Link
+                        href="https://chat.whatsapp.com/DOViTi8NHvH0LnapNsznoX"
+                        type="button"
+                        className="flex items-center space-x-2 rounded-lg bg-gray-700 px-6 py-3 font-silkscreen text-white transition-all hover:bg-gray-600"
+                      >
+                        Need Help ?
+                      </Link>
                       <button
                         type="button"
                         onClick={nextStep}
-                        className="flex items-center space-x-2 rounded-lg bg-gray-700 px-6 py-3 font-silkscreen text-white transition-all hover:bg-gray-600"
+                        disabled={isSubmitting}
+                        className="flex items-center space-x-2 rounded-lg bg-gray-700 px-6 py-3 font-silkscreen text-white transition-all hover:bg-gray-600 disabled:cursor-not-allowed disabled:opacity-70"
                       >
-                        <span>NEXT</span>
-                        <ChevronRight size={18} />
+                        {isSubmitting ? (
+                          <>
+                            <svg
+                              className="mr-2 size-5 animate-spin text-white"
+                              xmlns="http://www.w3.org/2000/svg"
+                              fill="none"
+                              viewBox="0 0 24 24"
+                            >
+                              <circle
+                                className="opacity-25"
+                                cx="12"
+                                cy="12"
+                                r="10"
+                                stroke="currentColor"
+                                strokeWidth="4"
+                              ></circle>
+                              <path
+                                className="opacity-75"
+                                fill="currentColor"
+                                d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                              ></path>
+                            </svg>
+                            <span>SUBMITTING...</span>
+                          </>
+                        ) : (
+                          <>
+                            <span>SUBMIT</span>
+                            <ChevronRight size={18} />
+                          </>
+                        )}
                       </button>
                     </div>
                   ) : (
-                    <div className="mt-8 flex justify-between">
+                    <div className="mt-8 flex flex-col items-center gap-4 md:flex-row md:justify-around">
                       <button
                         type="button"
                         onClick={prevStep}
@@ -1056,9 +1106,17 @@ export function RegistrationModal({
                         <ChevronLeft size={18} />
                         <span>BACK</span>
                       </button>
+                      <Link
+                        href="https://chat.whatsapp.com/DOViTi8NHvH0LnapNsznoX"
+                        type="button"
+                        className="flex items-center space-x-2 rounded-lg bg-gray-700 px-6 py-3 font-silkscreen text-white transition-all hover:bg-gray-600"
+                      >
+                        Need Help ?
+                      </Link>
                       <button
                         type="button"
                         onClick={nextStep}
+                        disabled={isSubmitting}
                         className="flex items-center space-x-2 rounded-lg bg-gray-700 px-6 py-3 font-silkscreen text-white transition-all hover:bg-gray-600 disabled:cursor-not-allowed disabled:opacity-70"
                       >
                         {isSubmitting ? (
@@ -1266,7 +1324,7 @@ export function RegistrationModal({
                       </div>
                     ))}
                   </div>
-                  <div className="mt-8 flex justify-between">
+                  <div className="mt-8 flex flex-col items-center gap-4 md:flex-row md:justify-around">
                     <button
                       type="button"
                       onClick={prevStep}
@@ -1275,6 +1333,13 @@ export function RegistrationModal({
                       <ChevronLeft size={18} />
                       <span>BACK</span>
                     </button>
+                    <Link
+                      href="https://chat.whatsapp.com/DOViTi8NHvH0LnapNsznoX"
+                      type="button"
+                      className="flex items-center space-x-2 rounded-lg bg-gray-700 px-6 py-3 font-silkscreen text-white transition-all hover:bg-gray-600"
+                    >
+                      Need Help ?
+                    </Link>
                     <button
                       type="button"
                       onClick={nextStep}
