79e643acd0912db958de54fb833010ce61e6b96b
E-Cell MJCET <ecellmjcet@mjcollege.ac.in>
2025-05-06 13:47:34 +0530

    Update registration-modal.tsx

--- components/hack-celerate/registration-modal.tsx
+++ components/hack-celerate/registration-modal.tsx
@@ -852,7 +852,7 @@ export function RegistrationModal({
                             before submission.
                           </span>
                         </li>
-                        <li className="flex items-start">
+{/*                         <li className="flex items-start">
                           <span className="mr-2 mt-1 shrink-0 text-[#7BF1A7]">
                             •
                           </span>
@@ -861,7 +861,7 @@ export function RegistrationModal({
                             teams will be charged Rs.70 per member and Rs. 100
                             for solo participants.
                           </span>
-                        </li>
+                        </li> */}
                         <li className="flex items-start">
                           <span className="mr-2 mt-1 shrink-0 text-[#7BF1A7]">
                             •
