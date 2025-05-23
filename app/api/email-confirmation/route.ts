dd6ce6f9b2fa3ec4730bccd4c8d937e72d45bde3
Mohammed Abid Nafi <abid.nafi04@gmail.com>
2025-05-23 20:05:04 +0530

    Add/husky (#64)
    * chore: add husky pre-commit hook for running tests
    
    * chore: add husky pre-commit hook for running tests
    
    * chore: add husky pre-commit hook for running tests
    
    * chore: add husky pre-commit hook for running tests
    
    * chore: add husky pre-commit hook for running tests
    
    * chore: add husky pre-commit hook for running tests
    
    * fix: resend error

--- app/api/email-confirmation/route.ts
+++ app/api/email-confirmation/route.ts
@@ -1,42 +1,42 @@
-import { Resend } from "resend";
-import { NextRequest, NextResponse } from "next/server";
+// import { Resend } from "resend";
+// import { NextRequest, NextResponse } from "next/server";
 
-interface EmailRequestBody {
-  to: string;
-  subject: string;
-  html: string;
-}
+// interface EmailRequestBody {
+//   to: string;
+//   subject: string;
+//   html: string;
+// }
 
-export const runtime = "edge";
+// export const runtime = "edge";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+// const resend = new Resend(process.env.RESEND_API_KEY);
 
-export async function POST(req: NextRequest) {
-  try {
-    const { to, subject, html }: EmailRequestBody = await req.json();
+// export async function POST(req: NextRequest) {
+//   try {
+//     const { to, subject, html }: EmailRequestBody = await req.json();
 
-    const response = await resend.emails.send({
-      from: "noreply@ecellmjcet.com",
-      to,
-      subject,
-      html,
-    });
-    console.log("Email sent successfully:", response);
+//     const response = await resend.emails.send({
+//       from: "noreply@ecellmjcet.com",
+//       to,
+//       subject,
+//       html,
+//     });
+//     console.log("Email sent successfully:", response);
 
-    return NextResponse.json(
-      { message: "Email sent", response },
-      { status: 200 }
-    );
-  } catch (error) {
-    console.error(error);
+//     return NextResponse.json(
+//       { message: "Email sent", response },
+//       { status: 200 }
+//     );
+//   } catch (error) {
+//     console.error(error);
 
-    return NextResponse.json(
-      { error: "Failed to send email" },
-      { status: 500 }
-    );
-  }
-}
+//     return NextResponse.json(
+//       { error: "Failed to send email" },
+//       { status: 500 }
+//     );
+//   }
+// }
 
-export function GET() {
-  return NextResponse.json({ error: "Method Not Allowed" }, { status: 405 });
-}
+// export function GET() {
+//   return NextResponse.json({ error: "Method Not Allowed" }, { status: 405 });
+// }
